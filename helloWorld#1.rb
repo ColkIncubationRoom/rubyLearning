7a8520b6f5980faf01634667d238322b88984ef5
Colk-tech <colk.tech@gmail.com>
2020-02-15 02:02:11 +0900

    firstProgram

---
+++ helloWorld#1.rb
@@ -0,0 +1,24 @@
+langName = "Ruby"
+lngName = 'Ruby'
+
+puts "Hello World!"
+
+puts "#{langName} #{lngName}"
+
+
+
+listExample = ["A","B","C",4]
+puts listExample[2]
+#コメントとインデックスの考え方自体はpythonと同じってことかな
+
+
+
+integer = 10
+
+if integer > 50
+  puts"integerは50より大きいですよ"
+elsif integer > 30
+  puts "integerは30よりは大きいですよ"
+else
+  puts "integerはどの条件にも当てはまらなかったよ"
+end
